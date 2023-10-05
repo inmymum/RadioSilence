7e6c28aa0a636f702bddd0ea17d5600a99a57d81
inmymum <114446605+inmymum@users.noreply.github.com>
2023-10-05 16:30:14 -0500

    Update main.lua

--- Releases/dev/main.lua
+++ Releases/dev/main.lua
@@ -18,6 +18,7 @@ function getPlayerNames() -- Get list of players for selection dropdown
 end
 
 -- Initialize tabs and sections
+local refresh = Window:NewTab("Refresh")
 local hubtab = Window:NewTab("Hubs")
 local movehub = Window:NewTab("Movement")
 local rapetab = Window:NewTab("Rape")
@@ -67,6 +68,9 @@ movement:NewButton("Default", "Set jump height to default", function()
     game.Players.LocalPlayer.Character.Humanoid.JumpPower = 18
 end)
 
+movement:NewButton("Refresh UI", "", function()
+    print("Refresh")
+end)
 -- Load UI
 hubs:UpdateSection("")
 rape:UpdateSection("")
